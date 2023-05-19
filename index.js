803eecd4d659120f738128e5884fe61251aadf0c
shuvojitsana <shuvojitsanabd2004@gmail.com>
2023-05-19 19:37:03 +0600

    toys has ben commited

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-console.log(process.env.DB_PASS)
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m8fciuv.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,12 +27,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db('sportsMaster').collection('toys');
+
+    
+   
+
+    app.get('/toys', async(req, res) =>{
+        const cursor = toysCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+    app.get('/toys/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id)}
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { title: 1, picture: 1, balance: 1,  },
+          };
+        const result = await toysCollection.findOne(query, options)
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
