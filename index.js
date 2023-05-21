270c14cc2ef4e15196f0f40aae4c622d8a08ad17
shuvojitsana <shuvojitsanabd2004@gmail.com>
2023-05-21 15:54:48 +0600

    added a index.js pages

--- index.js
+++ index.js
@@ -29,25 +29,65 @@ async function run() {
     await client.connect();
 
     const toysCollection = client.db('sportsMaster').collection('toys');
+    const sportsBookingCollection = client.db('sportsMaster').collection('sports');
 
-    
-   
 
-    app.get('/toys', async(req, res) =>{
-        const cursor = toysCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
+
+    app.get('/toys', async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
     })
 
-    app.get('/toys/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)}
-        const options = {
-            // Include only the `title` and `imdb` fields in the returned document
-            projection: { title: 1, picture: 1, balance: 1,  },
-          };
-        const result = await toysCollection.findOne(query, options)
-        res.send(result)
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, picture: 1, balance: 1, rating: 1, total_view: 1, about: 1, service_id: 1 },
+      };
+      const result = await toysCollection.findOne(query, options)
+      res.send(result)
+    })
+
+    // sportsBooking
+    app.get('/booking', async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email }
+      }
+      const result = await sportsBookingCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await sportsBookingCollection.insertOne(booking);
+      res.send(result);
+    })
+
+    app.patch('/booking/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updateBooking = req.body;
+      console.log(updateBooking);
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status
+        },
+      };
+      const result = await sportsBookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+
+    app.delete('/booking/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await sportsBookingCollection.deleteOne(query);
+      res.send(result)
     })
 
 
@@ -64,10 +104,10 @@ run().catch(console.dir);
 
 
 
-app.get('/',  (req, res) =>{
-    res.send("toys sports master running soon")
+app.get('/', (req, res) => {
+  res.send("toys sports master running soon")
 })
 
-app.listen(port, () =>{
-    console.log(`Sports Master server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Sports Master server is running on port ${port}`)
 })
